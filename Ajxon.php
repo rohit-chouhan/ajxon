163257d3fa284a1b30fe13dae52542dff0405d51
Rohit Chouhan <34239087+rohit-chouhan@users.noreply.github.com>
2020-09-04 19:32:57 +0530

    Update Ajxon.php

--- Ajxon.php
+++ Ajxon.php
@@ -6,7 +6,15 @@
 # LICENCE      : MIT
 #######################################
 error_reporting(0);
-class Ajxon {
+require 'cons/Input.php';
+require 'cons/Bootstrap.php';
+class Ajxon{
+	
+   use Input;
+   use Bootstrap;
+   
+   //---------------------------
+   
    var $json;
    var $input=array();
    var $js=array();
@@ -14,6 +22,7 @@ class Ajxon {
    var $data=array();
    var $ftype=array();
    var $fr=array();
+   var $stype=array();
    var $url;
    var $form_name;
    var $table;
@@ -21,7 +30,12 @@ class Ajxon {
 
     function set($j){
         $this->json=json_decode($j,true);
+		if($this->json==null){
+			header('location:cons/Err/Invalid.php');
+		}
     }
+	
+	
     function gen(){
             unset($this->input);
             unset($this->js);
@@ -29,22 +43,71 @@ class Ajxon {
             unset($this->data);
             unset($this->ftype);
             unset($this->fr);
+            unset($this->stype);
             $this->url=$this->json['url'];
             $this->form_name=$this->json['form'];
             $this->table=$this->json['table'];
             $this->dataType=$this->json['type'];
 
             foreach ($this->json['field'] as $key => $value) {
+				
+			unset($select);
+			unset($ss);
+			unset($fin);
+			
             $arr=explode(":",$value);
             $this->data[$key]=$arr[0];
             $this->ftype[]=$arr[1];
             $this->fr[]=$arr[2]=='true'?'required':'';
+			
+			if(isset($arr[3])){
+			$select[] = explode(",",str_replace(str_split('[]'),"",$arr[3]));
+			foreach ($select[0] as $x){
+			   $ss[]=explode("|",$x);
+		    }
+			
+			$myinput=empty($this->incls)?'':'class="'.$this->incls.'"';
+			$myinputstl=empty($this->instl)?'':'style="'.$this->instl.'"';
+		    for($i=0;$i<count($ss);$i++){
+				if($arr[1]=='radio') {
+					$fin[] = '<br>'. $ss[$i][1].' <input id="'.$ss[$i][0].'" name="'.$arr[0].'" type="radio" value="'.$ss[$i][0].'" '.$this->fr[$i].' />';
+				} else if($arr[1]=='select') {
+					$fin[] = '<option value="'.$ss[$i][0].'">'.$ss[$i][1].'</option>';
+			   }
+			}
+			
+			if($arr[1]=='radio') {
+					$this->stype[]=implode("",$fin);
+				} else if($arr[1]=='select') {
+					$this->stype[]='<select '.$myinput.' '.$myinputstl.' id="'.$arr[0].'">'.implode("",$fin).'</select>';
+			   }
+			
+			} else {
+				$this->stype[]='';
+			}
+			
             }
 
             $fcount=0;
+			
+			$myinput=empty($this->incls)?'':'class="'.$this->incls.'"';
+			$myinputstl=empty($this->instl)?'':'style="'.$this->instl.'"';
+			
             foreach ($this->data as $key => $value) {
-                $this->js[]= ' '.$key.':$("#'.$value.'").val()';
-                $this->input[]='<input id="'.$value.'" type="'.$this->ftype[$fcount].'" placeholder="Enter '.$value.'" '.$this->fr[$fcount].' />';
+				if($this->ftype[$fcount]=='radio') {
+					$this->js[]= ' '.$key.':$("#'.$value.':checked").val()';
+				} else if ($this->ftype[$fcount]=='select'){
+					$this->js[]= ' '.$key.':$("#'.$value.' option:selected").val()';
+				} else {
+					$this->js[]= ' '.$key.':$("#'.$value.'").val()';
+				}
+				if($this->stype[$fcount]==''){
+					$this->input[]=$this->bootformgroup.''.($this->bootlabel==true?'<label>'.ucfirst(str_replace("_"," ",$key)).'</label>':'').'<input id="'.$value.'" type="'.$this->ftype[$fcount].'" '.$myinput.' '.$myinputstl.' placeholder="Enter '.ucfirst(str_replace("_"," ",$key)).'" '.$this->fr[$fcount].' />'.$this->divclose;
+				} else {
+				//	$this->input[]=$this->stype[$fcount];
+					$this->input[]=$this->bootformgroup.''.($this->bootlabel==true?'<label>'.ucfirst(str_replace("_"," ",$key)).'</label>':'').' '.$this->stype[$fcount].'  '.$this->divclose;
+				}
+				
                 $this->columns[]=$key;
                 $this->values[]='\'".$_POST[\''.$key.'\']."\'';
                 $fcount+=1;
@@ -53,10 +116,14 @@ class Ajxon {
 
     function html(){
         $this->gen();
+		$mybtn=empty($this->btcls)?'':'class="'.$this->btcls.'"';
+		$mybtnstl=empty($this->btstl)?'':'style="'.$this->btstl.'"';
+		$mybtnnm=empty($this->btnam)?'Save':''.$this->btnam.'';
         $re='
+		'.$this->bootcard.'
         <form id="'.$this->json['form'].'" method="post" name="'.$this->json['form'].'">
-        '.implode("\n\t\t",$this->input)."\n".'<button type="submit">Save</button>
-         </form>';
+        '.implode("\n\t\t",$this->input)."\n".''.$this->bootformgroup.'<button '.$mybtnstl.' '.$mybtn.' type="submit">'.$mybtnnm.'</button>'.$this->divclose.'
+         </form>'.$this->divclose;
          echo $re;
     }
 
@@ -128,8 +195,8 @@ class Ajxon {
         }
 
         $file = $url;
-        $fp = fopen($file, "w") or die("Couldn't open $file for writing!");
-        fwrite($fp, $sql) or die("Couldn't write values to file!");
+        $fp = fopen($file, "w");
+        fwrite($fp, $sql);
         fclose($fp);
     }
 
